3532326f708546067dbba24910ee0f9ec5fa2e24
AbdulAahad13 <76912038+AbdulAahad13@users.noreply.github.com>
2021-05-19 23:51:46 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// const hamburger = document.getElementById('hamburger');
+// const navUl = document.querySelectorAll('navbar');
+
+
+// hamburger.addEventListener('click', () => {
+//     navUl.classlist.toggle('show');
+// })
